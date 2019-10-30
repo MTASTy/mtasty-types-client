b02fdda6aabfcec483b9ce5e037469e512266118
Evgeniy Shtro <augustalonso@gmail.com>
2019-10-30 16:35:13 +0200

    Add DxMaterial class declarations

--- class/dx/dx-material.d.ts
+++ class/dx/dx-material.d.ts
@@ -1,3 +1,9 @@
 declare class DxMaterial extends Element {
-
+  /**
+   * This gets the dimensions of the supplied material element.
+   * @returns Returns two numbers representing the width and height in pixels of the material. If the material is a volume texture, this function will return three numbers representing the width, height and depth.
+   * @see https://wiki.mtasa.com/wiki/DxGetMaterialSize
+   * @tupleReturn
+   **/
+  getSize(): [number, number, number];
 }
